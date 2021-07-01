7ef58a3682e7757957f2930ad8ff49757155c351
zdebil <zdebil@bbcag.ch>
2021-07-01 16:46:16 +0200

    demo vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="dynamic-component-demo" class="demo">
+      <button
+          v-for="tab in $data.tabs"
+          v-bind:key="tab"
+          v-bind:class="['tab-button', { active: currentTab === tab }]"
+          v-on:click="currentTab = tab"
+      >
+        {{ tab }}
+      </button>
+
+      <component v-bind:is="currentTabComponent" class="tab"></component>
+    </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import tabHome from './components/tabHome.vue'
+import tabPosts from './components/tabPosts.vue'
+import tabArchive from './components/tabArchive.vue'
 
-export default {
+export default({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, tabHome, tabPosts, tabArchive
+  },
+  data() {
+    return {
+      currentTab: 'Home',
+      tabs: ['Home', 'Posts', 'Archive']
+    }
+  },
+  computed: {
+    currentTabComponent() {
+      return 'tab-' + this.currentTab.toLowerCase()
+    }
   }
-}
+})
+
 </script>
 
 <style>
